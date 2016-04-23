75401e967e0a06ab3d6a8ba4c64cdf6cd875fac4
Ricardo Barros <ricardofbarros@hotmail.com>
2016-04-23 16:10:31 +0100

    refactor entry file

--- index.js
+++ index.js
@@ -1,113 +1,33 @@
-// Dependencies
-var CompositeDisposable = require('atom').CompositeDisposable
-var selectStyle = require('./utils/select-style')
-var styleSettings = require('./utils/style-settings')
+const atomPkgDeps = require('atom-package-deps')
+let config = require('./config')
+let Linter = require('./lib/linter')
+
+const scope = [
+  'source.js',
+  'source.js.jsx',
+  'source.js.jquery',
+  'source.gfm'
+]
+
+let linter = new Linter({
+  scope
+})
 
 module.exports = {
-  config: {
-    style: {
-      type: 'string',
-      default: 'standard',
-      enum: ['standard', 'semi-standard', 'happiness', 'uber-standard']
-    },
-    checkStyleDevDependencies: {
-      type: 'boolean',
-      title: 'Check for standard',
-      description: 'Only run if standard, semistandard or happiness present in package.json `devDependencies`',
-      default: false
-    },
-    honorStyleSettings: {
-      type: 'boolean',
-      description: 'Honor code style settings on package.json',
-      default: true
-    },
-    showEslintRules: {
-      type: 'boolean',
-      description: 'Show the eslint rule name on error/warning\'s message',
-      default: false
-    },
-    lintMarkdownFiles: {
-      type: 'boolean',
-      description: 'Lint markdown fenced code blocks',
-      default: false
+  config,
+  scope,
+  deactivate: linter.stopListening,
+  provideLinter: linter.provider,
+  activate: () => {
+    // Check if linter is listening to new events
+    // if it isn't start listen to
+    if (!linter.listening) {
+      linter.startListening()
     }
-  },
-  cache: new Map(),
-  subscriptions: {},
-  scope: ['source.js', 'source.js.jsx', 'source.js.jquery', 'source.gfm'],
-  activate: function () {
-    var self = this
 
     // Install linter-js-standard dependencies
-    require('atom-package-deps')
-    .install('linter-js-standard')
-    .then(function () {
-      var config = atom.config.get('linter-js-standard')
-      self.cache.set('config', config)
-
-      var storeSettings = function (paneItem) {
-        // Check if the pane is a file
-        if (!self.__checkIfTextEditor(paneItem)) {
-          return
-        }
-
-        // Get config
-        var config = self.cache.get('config')
-
-        // Check if this file is inside our grammar scope
-        var grammar = paneItem.getGrammar() || { scopeName: null }
-        if (self.scope.indexOf(grammar.scopeName) < 0 || (!config.lintMarkdownFiles && grammar.scopeName === 'source.gfm')) {
-          return
-        }
-
-        // Cache active pane
-        self.__cacheTextEditor(config, paneItem)
-      }
-
-      // On startup get active pane
-      // check if it's a text editor
-      // if it is cache it's settings
-      var paneItem = atom.workspace.getActivePaneItem()
-      storeSettings(paneItem)
-
-      // Create some subscriptions
-      self.subscriptions = new CompositeDisposable()
-
-      self.subscriptions.add(atom.workspace.onDidChangeActivePaneItem(storeSettings))
-
-      // on package settings change
-      self.subscriptions.add(atom.config.observe('linter-js-standard', function (config) {
-        // Cache config
-        self.cache.set('config', config)
-      }))
-    })
-  },
-
-  deactivate: function () {
-    this.subscriptions.dispose()
-  },
-
-  __cacheTextEditor: function (config, textEditor) {
-    var filePath = textEditor.getPath()
-    var style = selectStyle(config, filePath)
-    var opts = {}
-
-    // If setting honorStyleSettings is checked
-    // and there is a valid linter
-    if (config.honorStyleSettings && style && style.cmd !== 'no-style') {
-      // This function may modify the following variables:
-      // - opts
-      // - style
-      styleSettings.call({ args: opts, style: style }, filePath)
-    }
-
-    // Cache style settings and args of some file
-    this.cache.set('text-editor', { style: style, opts: opts })
-  },
-
-  __checkIfTextEditor: function (paneItem) {
-    return (paneItem && typeof paneItem.getGrammar === 'function' && typeof paneItem.getPath === 'function')
-  },
-
-  provideLinter: require('./linter-js-standard')
+    // then run linter initialize fn
+    atomPkgDeps.install('linter-js-standard')
+      .then(linter.initialize)
+  }
 }
